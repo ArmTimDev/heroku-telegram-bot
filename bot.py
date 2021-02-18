5b2999fed8bf6b1f976e8791ca91966fdbaa9f05
ArmTimDev <76068426+ArmTimDev@users.noreply.github.com>
2021-02-18 14:01:32 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,20 +1,49 @@
-# -*- coding: utf-8 -*-
-import redis
 import os
-import telebot
-# import some_api_lib
-# import ...
+import sys
+from threading import Thread
+from telegram.ext import Updater, CommandHandler, Filters
 
-# Example of your code beginning
-#           Config vars
+from handlers import all_handlers
+
 token = os.environ['TELEGRAM_TOKEN']
-some_api_token = os.environ['SOME_API_TOKEN']
-#             ...
 
-# If you use redis, install this add-on https://elements.heroku.com/addons/heroku-redis
-r = redis.from_url(os.environ.get("REDIS_URL"))
+updater = Updater(token)
+for handler in all_handlers:
+    if len(handler) == 2:
+        updater.dispatcher.add_handler(
+            handler[0],
+            handler[1]
+        )
+    else:
+        updater.dispatcher.add_handler(
+            handler[0]
+        )
 
-#       Your bot code below
-# bot = telebot.TeleBot(token)
-# some_api = some_api_lib.connect(some_api_token)
-#              ...
+
+def stop_and_restart():
+    updater.stop()
+    os.system("git pull")
+    os.execl(sys.executable, sys.executable, *sys.argv)
+
+
+def restart(update, contex):
+    update.message.reply_text("Restarting Vippy...")
+    Thread(
+        target=stop_and_restart
+    ).start()
+
+
+updater.dispatcher.add_handler(
+    CommandHandler(
+        "restart",
+        restart,
+        filters=Filters.user(
+            [
+                #Sudo Users
+                
+            ]
+        )
+    )
+)
+updater.start_polling(clean=True)
+updater.idle()
